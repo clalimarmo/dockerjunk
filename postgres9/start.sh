db99dfd26b09d87e21ccb5f1e3ea41ed4bfb0087
Carlos Lalimarmo <clalimarmo@gmail.com>
2019-03-05 13:45:39 -0500

    adjust postgres9 data mount location

--- postgres9/start.sh
+++ postgres9/start.sh
@@ -4,7 +4,7 @@ mkdir -p $host_data_dir
 
 docker run \
 	--name postgres9 \
-	--mount type=bind,source=$host_data_dir,target=/var/lib/postgresql/data \
+	--mount type=bind,source=$host_data_dir,target=$HOME/var/postgres9/data \
 	--shm-size=256 \
 	-p 5432:5432 \
 	-d postgres:9.6.12
