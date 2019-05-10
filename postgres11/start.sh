023f14399391e12a3a236b3bea1ca68e6ba61a52
Carlos Lalimarmo <clalimarmo@gmail.com>
2019-05-10 16:38:16 -0400

    fixup

--- postgres11/start.sh
+++ postgres11/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-host_data_dir=/tmp/dockerjunk/postgres11/data
+host_data_dir="$HOME/var/postgres11/data"
 mkdir -p $host_data_dir
 
 docker run \
 	--name postgres11 \
-	--mount type=bind,source=$host_data_dir,target=$HOME/var/postgres11/data \
+	--mount type=bind,source=$host_data_dir,target=/var/lib/postgresql/data \
 	--shm-size=512m \
 	-p 5432:5432 \
 	-d postgres:11
